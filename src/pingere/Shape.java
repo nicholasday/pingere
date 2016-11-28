75e99523414c77a902eb9ef55add41645dcdbcfc
Nicholas Day <nick@nickendo.com>
2016-11-28 00:36:17 -0500

    Added official rectangles and ellipses

---
+++ src/pingere/Shape.java
@@ -0,0 +1,33 @@
+package pingere;
+
+import java.awt.Color;
+
+public class Shape
+{
+	private int x = 0;
+	private int y = 0;
+	private int width = 0;
+	private int height = 0;
+	private Type type;
+	private Color color;
+	
+	enum Type {
+		Rectangle, Ellipse
+	}
+	
+	public Shape(int x, int y, int width, int height, Type type, Color color) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.type = type;
+		this.color = color;
+	}
+	
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public int getWidth() { return width; }
+	public int getHeight() { return height; }
+	public Type getType() { return type; }
+	public Color getColor() { return color; }
+}
---
+++ src/pingere/ShapeDraw.java
@@ -0,0 +1,110 @@
+package pingere;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class ShapeDraw extends JPanel
+{
+	private int startX;
+	private int startY;
+	private int endX;
+	private int endY;
+	private int x, y;
+	private int height;
+	private int width;
+	
+	private ArrayList<Shape> shapes = new ArrayList<Shape>();
+	
+	public ShapeDraw() {
+		setBackground(Color.white);
+		addMouseListener(new MouseAdapter());
+		addMouseMotionListener(new MouseMotionAdapter());
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+
+		for (Shape shape: shapes) {
+			g2d.setColor(shape.getColor());
+
+			switch(shape.getType()) {
+				case Ellipse:
+					g2d.draw(new Ellipse2D.Double(shape.getX(), shape.getY(), shape.getWidth(), shape.getHeight()));
+					break;
+				case Rectangle:
+					g2d.draw(new Rectangle2D.Double(shape.getX(), shape.getY(), shape.getWidth(), shape.getHeight()));
+					break;
+				default:
+					break;
+			}
+		}
+
+		g2d.setColor(State.getColor());
+		
+		switch(State.getTool()) {
+			case Brush:
+				break;
+			case Ellipse:
+				g2d.draw(new Ellipse2D.Double(x, y, width, height));
+				break;
+			case Eraser:
+				break;
+			case Rectangle:
+				g2d.draw(new Rectangle2D.Double(x, y, width, height));
+				break;
+			default:
+				break;
+		}
+	}
+
+	private class MouseMotionAdapter implements MouseMotionListener {
+		public void mouseMoved(MouseEvent event) { }
+
+		public void mouseDragged(MouseEvent event) {
+			endX = event.getX();
+			endY = event.getY();
+			
+			x = Math.min(startX, endX);
+			y = Math.min(startY, endY);
+
+			height = Math.abs(startY - endY);
+			width = Math.abs(startX - endX);
+			repaint();
+		}
+	}
+
+	private class MouseAdapter implements MouseListener {
+		public void mouseClicked(MouseEvent event) { }
+		public void mouseEntered(MouseEvent event) { }
+		public void mouseExited(MouseEvent event) { }
+
+		public void mouseReleased(MouseEvent event) { 
+			switch(State.getTool()) {
+				case Brush:
+					break;
+				case Ellipse:
+					Shape ellipse = new Shape(x, y, width, height, Shape.Type.Ellipse, State.getColor());
+					shapes.add(ellipse);
+					break;
+				case Eraser:
+					break;
+				case Rectangle:
+					Shape rectangle = new Shape(x, y, width, height, Shape.Type.Rectangle, State.getColor());
+					shapes.add(rectangle);
+					break;
+				default:
+					break;
+			}
+		}
+
+		public void mousePressed(MouseEvent event) {
+			startX = event.getX();
+			startY = event.getY();
+		}
+	}
+}
