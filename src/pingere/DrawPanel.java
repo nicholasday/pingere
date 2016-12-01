8b3503776bbbc704caad1300199b0412ad0aef8a
Nicholas Day <nick@nickendo.com>
2016-12-01 00:19:03 -0500

    Finished comments for DrawPanel

--- src/pingere/DrawPanel.java
+++ src/pingere/DrawPanel.java
@@ -132,92 +132,124 @@ public class DrawPanel extends JPanel {
 					g2d.drawImage(chafic, shape.getX(), shape.getY(), null);
 				default:
 					break;
-				}
+			}
 		}
 
+		// This is where we draw our "in-progress" shapes or shapes that are 
+		// being resized by the user when the mouse click hasn't been 
+		// released
 		g2d.setColor(State.getColor());
 
+		// The if statement is to make sure to not do the redraw shapes 
+		// right after the image has been cleared. Otherwise when you hit 
+		// clear it will clear all but the last shape
 		if (!State.isClear()) {
 			switch (State.getTool()) {
-			case Brush:
-				break;
-			case Ellipse:
-				g2d.setStroke(new BasicStroke(State.getStroke()));
-				g2d.draw(new Ellipse2D.Double(x, y, width, height));
-				break;
-			case Eraser:
-				break;
-			case Rectangle:
-				g2d.setStroke(new BasicStroke(State.getStroke()));
-				g2d.draw(new Rectangle2D.Double(x, y, width, height));
-				break;
-			default:
-				break;
+				case Brush:
+					break;
+				case Ellipse:
+					g2d.setStroke(new BasicStroke(State.getStroke()));
+					g2d.draw(new Ellipse2D.Double(x, y, width, height));
+					break;
+				case Eraser:
+					break;
+				case Rectangle:
+					g2d.setStroke(new BasicStroke(State.getStroke()));
+					g2d.draw(new Rectangle2D.Double(x, y, width, height));
+					break;
+				default:
+					break;
 			}
 		}
 	}
 
+	// Here we create a private MotionMouseListener class based
+	// on MouseMotionListener interface to get when the mouse has been
+	// dragged to get resizable shapes
 	private class MouseMotionAdapter implements MouseMotionListener {
-		public void mouseMoved(MouseEvent event) {
-		}
+		// Required for the MouseMotionListener interface
+		public void mouseMoved(MouseEvent event) { }
 
+		// Here's where the magic happens. We get the current end point of the mouse
+		// to redraw and draw lines
 		public void mouseDragged(MouseEvent event) {
+			// Gets the x and y pos from mouse
 			endX = event.getX();
 			endY = event.getY();
 
+			// If we are using the brush we draw a line from start to finish then 
+			// set the start of the next line to be the end of the current one. 
+			// This way we draw an almost infinite number of lines as the user drags
+			// their mouse but it comes together to be seen as one.
 			if (State.getTool() == State.Tool.Brush) {
 				Shape line = new Shape(startX, startY, endX, endY, State.getColor(), Shape.Type.Brush, State.getStroke());
 				shapes.add(line);
 
 				startX = endX;
 				startY = endY;
+
+			// The same logic applies for Eraser, but the size is bigger and it's white 
+			// to blend into the background and supersede the previous shapes it covered
 			} else if (State.getTool() == State.Tool.Eraser) {
 				Shape erased = new Shape(startX, startY, endX, endY, State.getColor(), Shape.Type.Eraser, State.getStroke());
 				shapes.add(erased);
 
 				startX = endX;
 				startY = endY;
+				
+			// Here we find the coordinates of the redrawable shapes (ellipse and rectangle)
 			} else {
+				// We use Math.min for startX and endX (and Y) because we want to support
+				// the user drawing the shape in a direction before the startX/Y which 
+				// would mean that the place where we start drawing (the top left corner
+				// of the shape) is actually endX/Y not startX/Y
 				x = Math.min(startX, endX);
 				y = Math.min(startY, endY);
 
+				// We use Math.abs because we want to support the user drawing in any
+				// direction. The height and width will always be the same regardless
+				// of direction and orientation
 				height = Math.abs(startY - endY);
 				width = Math.abs(startX - endX);
 			}
 
+			// We repaint at the end to ensure that the user's dragging changes are updated
 			repaint();
 		}
 	}
 
+	// We create MouseAdapter which implements MouseListener so that we can
+	// get the start point of when the user started dragging and the release point
+	// so that we can draw the final shape
 	private class MouseAdapter implements MouseListener {
-		public void mouseClicked(MouseEvent event) {
-		}
+		// Required for the MouseListener interface
+		public void mouseClicked(MouseEvent event) { }
+		public void mouseEntered(MouseEvent event) { }
+		public void mouseExited(MouseEvent event) { }
 
-		public void mouseEntered(MouseEvent event) {
-		}
-
-		public void mouseExited(MouseEvent event) {
-		}
-
+		// Code that creates the shape and adds it to the shapes array
+		// upon mouse release
 		public void mouseReleased(MouseEvent event) {
 			switch (State.getTool()) {
-			case Brush:
-				break;
-			case Ellipse:
-				Shape ellipse = new Shape(x, y, width, height, Shape.Type.Ellipse, State.getColor());
-				shapes.add(ellipse);
-				break;
-			case Eraser:
-				break;
-			case Rectangle:
-				Shape rectangle = new Shape(x, y, width, height, Shape.Type.Rectangle, State.getColor());
-				shapes.add(rectangle);
-				break;
-			default:
-				break;
+				case Brush:
+					break;
+				case Ellipse:
+					Shape ellipse = new Shape(x, y, width, height, Shape.Type.Ellipse, State.getColor());
+					shapes.add(ellipse);
+					break;
+				case Eraser:
+					break;
+				case Rectangle:
+					Shape rectangle = new Shape(x, y, width, height, Shape.Type.Rectangle, State.getColor());
+					shapes.add(rectangle);
+					break;
+				default:
+					break;
 			}
 		}
 
+		// Here we get the startX/Y for dragging, and if we have the 
+		// special image selected, we draw the image immediately
 		public void mousePressed(MouseEvent event) {
 			startX = event.getX();
 			startY = event.getY();
@@ -229,9 +261,14 @@ public class DrawPanel extends JPanel {
 		}
 	}
 
+	// This is a public function so that ToolbarPanel can have a button
+	// that clears the image immediately and trigger a repaint
 	public void clear() {
+		// Creates a white shape the size of the window to blank the screen
 		Shape clear = new Shape(0, 0, getSize().width, getSize().height, Shape.Type.Clear, Color.white);
 		shapes.add(clear);
+		
+		// Forces a repaint
 		repaint();
 	}
 }
