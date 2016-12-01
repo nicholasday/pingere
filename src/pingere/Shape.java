37c6c602e4ea97485616b0c9020bcaddd9913f59
Nicholas Day <nick@nickendo.com>
2016-11-30 23:02:50 -0500

    Added comments for shape

--- src/pingere/Shape.java
+++ src/pingere/Shape.java
@@ -1,23 +1,45 @@
 package pingere;
 
+// Used to save the line width
 import java.awt.BasicStroke;
+
+// Used to save the color of the shape
 import java.awt.Color;
 
 public class Shape {
+	// x and y store the left corner position that the shape draws
+	// from and the start of the line for a line shape
 	private int x = 0;
 	private int y = 0;
+	
+	// x2 and y2 are the end points for the line shape
 	private int x2 = 0;
 	private int y2 = 0;
+	
+	// width and height are for the width and height of an ellipse 
+	// which would be passed to their draw functions
 	private int width = 0;
 	private int height = 0;
+	
+	// Type stores the type of the shape based on the enum defined
+	// below
 	private Type type;
+	
+	// Stores the color of the shape
 	private Color color;
+	
+	// Stores the line width of the shape
 	private BasicStroke stroke;
 
-	enum Type {
+	// This enum serves as a list of shapes we can store
+	public enum Type {
 		Rectangle, Ellipse, Brush, Eraser, Clear, Chafic
 	}
+	
+	// We have multiple constructors because we have many different types
+	// of shapes and want to support them all without passing in null values
 
+	// This is the constructor for rectangle and ellipse shapes
 	public Shape(int x, int y, int width, int height, Type type, Color color) {
 		this.x = x;
 		this.y = y;
@@ -27,6 +49,7 @@ public class Shape {
 		this.color = color;
 	}
 
+	// This is the constructor for lines that width's are default
 	public Shape(int x, int y, int x2, int y2, Color color) {
 		this.x = x;
 		this.y = y;
@@ -37,6 +60,7 @@ public class Shape {
 		this.stroke = new BasicStroke(1);
 	}
 
+	// This is the constructor for lines that width's that change
 	public Shape(int x, int y, int x2, int y2, Color color, BasicStroke stroke) {
 		this.x = x;
 		this.y = y;
@@ -47,11 +71,21 @@ public class Shape {
 		this.stroke = stroke;
 	}
 
+	// This is the constructor for the special image
 	public Shape(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.type = Type.Chafic;
 	}
+
+	//----------------------------------//
+	// 			Arcessors 				//
+	//----------------------------------//
+	//									// 
+	// These access the values of the   //
+	// shape so that we can draw them   //
+	// 									//
+	//----------------------------------//
 
 	public int getX() {
 		return x;
